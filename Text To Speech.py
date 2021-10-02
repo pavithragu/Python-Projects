af3f34dcf052aafbb5b6a2cd20d395b57557531b
Ragu M <90707812+pavithragu@users.noreply.github.com>
2021-10-02 13:47:52 +0530

    First Commit of Text to Speech
    Text to speech converts the text into an audio file...
    It supports multilanguages...

---
+++ Text To Speech.py
@@ -0,0 +1,18 @@
+﻿from gtts import gTTS
+import os
+
+# Open function to open & read the file
+source = "தமிழ் வாழ்க"
+
+# Language in which you want to convert
+language = "ta"
+
+# Passing the text and language to the engine, here we have marked slow=False. Which tells the module that the
+# converted audio should have a high speed
+file = gTTS(text=source, lang=language, slow=False)
+
+# Saving the converted audio in a mp3 file
+file.save("output.mp3")
+
+# Playing the converted file
+os.system("output.mp3")
